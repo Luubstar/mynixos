47fd678885ba2601d6c777bbea09c8a85170dfaa
Luubstar <nbaronariera@gmail.com>
2024-12-25 17:29:51 +0100

    commit autogenerated when updating

--- nixosModules/default.nix
+++ nixosModules/default.nix
@@ -6,6 +6,8 @@
     ./programacion/git.nix
     ./programacion/nixfmt.nix
     ./programacion/virtualizacion.nix
+    ./programacion/rust.nix
+    ./programacion/rustrover.nix
     ./hyprland/cachix.nix
     ./hyprland/hyprlandModule.nix
   ];

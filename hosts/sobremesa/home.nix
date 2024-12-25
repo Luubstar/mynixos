1f44796f873e22efda66927f2f309ace8b095e03
Luubstar <nbaronariera@gmail.com>
2024-12-25 14:12:28 +0100

    commit autogenerated when updating

--- hosts/sobremesa/home.nix
+++ hosts/sobremesa/home.nix
@@ -73,7 +73,7 @@
   #
   #  /etc/profiles/per-user/nbr/etc/profile.d/hm-session-vars.sh
 
-  my.enableAllHyprland = false;
+  my.enableAllHyprland = true;
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }

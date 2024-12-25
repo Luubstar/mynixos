83b88b2f5cd5b9f46001f0506968e65761007067
Luubstar <nbaronariera@gmail.com>
2024-12-25 13:41:10 +0100

    commit autogenerated when updating

--- hosts/sobremesa/home.nix
+++ hosts/sobremesa/home.nix
@@ -15,6 +15,11 @@
   # release notes.
   home.stateVersion = "24.11"; # Please read the comment before changing.
 
+
+  imports = [
+    ../../homeManagerModules/default.nix
+  ]
+
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [

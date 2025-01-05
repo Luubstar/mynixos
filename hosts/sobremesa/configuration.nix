dff99f08ce09286322717881c20ec5f07f853267
Luubstar <nbaronariera@gmail.com>
2025-01-05 11:16:54 +0100

    commit autogenerated when updating

--- hosts/sobremesa/configuration.nix
+++ hosts/sobremesa/configuration.nix
@@ -116,7 +116,7 @@
     pulse.enable = true;
     jack.enable = true;
     audio.enable = true;
-    manager = "wireplumber";
+    #manager = "wireplumber";
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
